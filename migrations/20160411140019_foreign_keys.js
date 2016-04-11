4b466e197ba277c6479a9176cb2c8e8df0d12156
Mike Homolak <mhomolak@yahoo.com>
2016-04-11 14:35:35 -0700

    DB and tables setup.  GIT DUET MIKE AND TONY IF THIS DIDNTWORK

---
+++ migrations/20160411140019_foreign_keys.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('messages', function(table) {
+    table.increments('');
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+    table.datetime('date');
+    table.text('body');
+  })
+  .createTable('questions', function(table) {
+    table.increments('');
+    table.text('body');
+    table.string('title');
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+    table.datetime('date');
+  })
+  .createTable('replies', function(table) {
+    table.increments('');
+    table.text('body');
+    table.datetime('date');
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+    table.integer('question_id').references('id').inTable('questions').onDelete('CASCADE').onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('messages').dropTable('questions').dropTable('replies');
+};
