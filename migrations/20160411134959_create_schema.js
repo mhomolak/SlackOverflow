7546d029ff5da32b6b061ea6166675baa9c2c8ed
Anthony Simpson <ansi6622@colorado.edu>
2016-04-12 15:17:22 -0600

    sooooo fuggin sorryyyyyyyy

--- migrations/20160411134959_create_schema.js
+++ migrations/20160411134959_create_schema.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
     table.string('name');
     table.string('password');
     table.string('email');
-    table.timestamps('date');
+    table.timestamp('created_at').defaultTo(knex.raw('now()'));
     table.boolean('admin').defaultTo('false');
     table.string('portrait_url');
     table.string('github_profile_url');
--- migrations/20160411140019_foreign_keys.js
+++ migrations/20160411140019_foreign_keys.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('messages', function(table) {
     table.increments('');
     table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
-    table.timestamps('date');
+    table.timestamp('created_at').defaultTo(knex.raw('now()'));
     table.text('body');
   })
   .createTable('questions', function(table) {
@@ -11,12 +11,12 @@ exports.up = function(knex, Promise) {
     table.text('body');
     table.string('title');
     table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
-    table.timestamps('date');
+    table.timestamp('created_at').defaultTo(knex.raw('now()'));
   })
   .createTable('replies', function(table) {
     table.increments('');
     table.text('body');
-    table.timestamps('date');
+    table.timestamp('created_at').defaultTo(knex.raw('now()'));
     table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
     table.integer('question_id').references('id').inTable('questions').onDelete('CASCADE').onUpdate('CASCADE');
   })
