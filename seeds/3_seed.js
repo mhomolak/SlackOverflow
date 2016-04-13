7b65f9d0f5268680bee908c8a8d802e09582e20f
Anthony Simpson <ansi6622@colorado.edu>
2016-04-13 02:04:47 -0600

    added some extra data to be inserted into seedfiles for visualization of tag relationships

--- seeds/3_seed.js
+++ seeds/3_seed.js
@@ -15,8 +15,14 @@ exports.seed = function(knex, Promise) {
 
   var tags_usersArr = [
     {user_id: 1, tag_id:3},
+    {user_id: 1, tag_id:6},
     {user_id: 2, tag_id:2},
-    {user_id: 3, tag_id:1}
+    {user_id: 3, tag_id:1},
+    {user_id: 3, tag_id:3},
+    {user_id: 3, tag_id:2},
+    {user_id: 2, tag_id:5},
+    {user_id: 2, tag_id:4}
+
   ]
 
   var channels_usersArr = [
