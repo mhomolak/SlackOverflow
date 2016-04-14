64cb9b1f009b7679856a59cab6bccdd72d8ea1a2
James Freeman <freeman.james.h@gmail.com>
2016-04-14 12:39:15 -0600

    throw admin cookie

--- routes/auth.js
+++ routes/auth.js
@@ -58,6 +58,7 @@
       }).first().then(function(user) {
         if (user && bcrypt.compareSync(req.body.password, user.password)) {
           req.session.email = user.email;
+          req.session.admin = user.admin;
           res.redirect('../users');
         } else {
           res.redirect('/login');
